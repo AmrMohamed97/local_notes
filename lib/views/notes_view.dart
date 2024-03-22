4000f24fb69a1f9524789fd9957761ad87a4b779
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-03-22 17:15:42 +0200

    first version of note

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,9 +6,61 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-      ),
+      body: NoteViewBody(),
     );
+  }
+}
+
+class NoteViewBody extends StatelessWidget {
+  const NoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0),
+      child: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    ));
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          color: Colors.white.withOpacity(.05),
+        ),
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ));
   }
 }
