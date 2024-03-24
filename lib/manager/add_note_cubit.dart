7abeecb0dcafda97a6cf179acf2c465c25201e5e
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-03-24 07:54:33 +0200

    add cubit

--- lib/manager/add_note_cubit.dart
+++ lib/manager/add_note_cubit.dart
@@ -1,1 +1,6 @@
+import 'package:bloc/bloc.dart';
+import 'package:local_notes_app/manager/add_note_state.dart';
 
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitialState());
+}
