30cbac06e87a07afb344bc09e3cd524190bcfa49
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-03-24 07:43:16 +0200

    fix-main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:local_notes_app/core/models/note_model.dart';
 import 'package:local_notes_app/core/utiles/constants.dart';
-import 'package:local_notes_app/core/utiles/my_binding.dart';
-import 'package:local_notes_app/views/edit_note/edit_note_view.dart';
 import 'package:local_notes_app/views/note_view/notes_view.dart';
 
 void main() async {
@@ -22,17 +19,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         fontFamily: 'Poppins',
       ),
-      routes: {
-        NotesView.id: (context) => const NotesView(),
-        EditNoteVew.id: (context) => const EditNoteVew(),
-      },
-      initialBinding: MyBinding(),
+      home: const NotesView(),
     );
   }
 }
